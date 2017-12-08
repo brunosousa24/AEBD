a3473b7f92d234fc5b2a8022a7a1eb47bcfa3560
Bruno Sousa <brunosousa@Brunos-MBP.lan>
2017-12-08 13:38:39 +0000

    conection with database working

--- AEBD/src/Conection.java
+++ AEBD/src/Conection.java
@@ -54,17 +54,17 @@ public class Conection {
 Class.forName("oracle.jdbc.driver.OracleDriver");  
   
 //step2 create  the connection object  
-Connection con=DriverManager.getConnection(  
-"jdbc:oracle:User1@localhost:1521:orcl","system","oracle");  
+Connection con=DriverManager.getConnection("jdbc:oracle:thin:hr/hr@localhost:1521/orcl","hr","oracle");  
   
 //step3 create the statement object  
 
 Statement stmt=con.createStatement();  
   
 //step4 execute query  
-ResultSet rs=stmt.executeQuery("select * from emp");  
-while(rs.next())  
-System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+ResultSet rs=stmt.executeQuery("select * from JOBS");  
+System.out.println("JOBS:");
+while(rs.next())
+System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getFloat(3)+"\t"+rs.getFloat(4));  
   
 //step5 close the connection object  
 con.close();  
