546536f2ebcba0765ce846b01b0b3ba01ae05310
ricardocerto16 <a75315@alunos.uminho.pt>
2018-01-15 23:25:28 +0000

    add bd adicional para performance

---
+++ Antigo/Povoamento/hospital.sql
@@ -0,0 +1,7 @@
+INSERT INTO HOSPITAL VALUES (1,'Hospital de Braga','Braga','Público','222555333');
+INSERT INTO HOSPITAL VALUES (2,'Hospital Privado de Braga','Braga','Privado','222444333');
+INSERT INTO HOSPITAL VALUES (3,'Hospital S.João','Porto','Público','212258789');
+INSERT INTO HOSPITAL VALUES (4,'Hospiatl da Luz','Guimarães','Público','201201548');
+INSERT INTO HOSPITAL VALUES (5,'Hospital de Santa Maria','Lisboa','Público','277892258');
+INSERT INTO HOSPITAL VALUES (6,'Hospital da Trofa','Trofa','Público','201202203');
+INSERT INTO HOSPITAL VALUES (7,'IPO','Porto','Público','200589500');
